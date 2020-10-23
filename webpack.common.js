69e05cf278e1358d8580cf4b89f4e5990c83df2d
sabovyan <abovyan.sargis90@gmail.com>
2020-10-23 12:31:19 +0400

    ci(webpack) : split the code for 2 different modes (prod, dev)

--- webpack.common.js
+++ webpack.common.js
@@ -1,13 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle[contenthash].js',
-  },
 
   plugins: [
     new HtmlWebpackPlugin({
