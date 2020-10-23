0ffdc2b0f191878b63701a1b62b8f98a6af3a513
sabovyan <abovyan.sargis90@gmail.com>
2020-10-23 13:22:10 +0400

    ci(webpack) : add webpack dev server

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,14 @@
-const common = require('./webpack.common');
-const { merge } = require('webpack-merge');
 const path = require('path');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'development',
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 8000,
+  },
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,6 @@
-const common = require('./webpack.common');
-const { merge } = require('webpack-merge');
 const path = require('path');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'production',
